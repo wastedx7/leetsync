199c121883ca956af3939cb2cb8647a177abf12d
Anugrah Umarvanshi <wastedx7@gmail.com>
2026-09-17 19:21:52 +0530

    Time: 2 ms (72.99%) | Memory: 43 MB (27.37%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int sl = 0;
+        int tl = 0;
+
+        while(sl < s.length() && tl < t.length()){
+            if(s.charAt(sl) == t.charAt(tl)){
+                sl++;
+            }
+            tl++;
+        }
+        return sl == s.length();
+    }
+}
